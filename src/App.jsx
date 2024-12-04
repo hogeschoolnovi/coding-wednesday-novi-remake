90ecb8cfd9581313b9ad76f4124fd143a8b7c4fe
Daniel Stam <daniel.stam@live.nl>
2024-12-04 17:19:35 +0100

    wijzigingen gedaan!

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,6 @@ function App() {
                         />
                         <ItemLabel
                             title='6mnd'
-
                             text='gem. vrijstelling'
                         />
                         <ItemLabel
--- src/components/itemlabal/ItemLabel.jsx
+++ src/components/itemlabal/ItemLabel.jsx
@@ -3,15 +3,11 @@ import './ItemLabel.css'
 
 const ItemLabel = ({title, text}) => {
     return (
-        <>
-            <article className='article'>
-                <h2 className='h2'>{title}</h2>
-
-                <p className='p'>{text}</p>
-
+        <article className="article">
+                <h2 className="h2">{title}</h2>
+                <p className="p">{text}</p>
             </article>
-        </>
-    );
+           );
 };
 
 export default ItemLabel;
