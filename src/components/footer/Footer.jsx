84c963fcdbdfd74e8f143c3cb67f61da15b6a91c
RowanPl <Rowanplooij@hotmail.com>
2024-12-04 16:37:37 +0100

    made some changes to use the ResourceLinks

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,54 +1,42 @@
 import React from 'react';
 import List from "../list/List.jsx";
 import InputField from "../inputField/InputField.jsx";
+import ResourceLinks from "../resourceLinks/ResourceLinks.jsx";
 
 const Footer = () => {
     return (
         <div>
             <img src="https://www.novi.nl/hubfs/Website/NOVI%20logo%20wit.svg" alt="Logo of NOVI Hogeschool"/>
             <div>
-
-
-                <List
-                    title="Over NOVI">
-                    <a href="https://www.test.nl" target="_blank">Opleidingen</a>
-                    <a href="https://www.test.nl" target="_blank">Studeren bij</a>
-                    <a href="https://www.test.nl" target="_blank">Blog</a>
-                    <a href="https://www.test.nl" target="_blank">Contact</a>
-                    <a href="https://www.test.nl" target="_blank">Vacatures</a>
+                <List title="Over NOVI">
+                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Opeidingen" />
+                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Studeren Bij" />
+                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Blog" />
+                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Contact" />
+                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Vacatures"/>
                 </List>
-                <List
-                    title="Studenten">
-                    <a href="https://www.test.nl" target="_blank">Kennisbank</a>
-                    <a href="https://www.test.nl" target="_blank">EdHub</a>
-                    <a href="https://www.test.nl" target="_blank">Subsidie</a>
-                    <a href="https://www.test.nl" target="_blank">Verkort HBO-Traject</a>
+                <List title="Studenten">
+                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Kennisbank" />
+                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Edhub"/>
+                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Subsidie"/>
+                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Verkort HBO-traject"/>
                 </List>
-                <List
-                    title="In Company">
-                    <a href="https://www.test.nl" target="_blank">Business</a>
-                    <a href="https://www.test.nl" target="_blank">Financiering</a>
+                <List title="In Company">
+                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Business" />
+                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Financiering"/>
                 </List>
-
             </div>
             <div>
-                <List
-                    title="Contact">
-                    <a href="https://www.test.nl" target="_blank">Email</a>
-                    <a href="https://www.test.nl" target="_blank">Telefoonnummer</a>
-                    <a href="https://www.test.nl" target="_blank">Adres</a>
-
-
-
-                    <a href="https://www.test.nl" target="_blank">Twitter</a>
-                    <a href="https://www.test.nl" target="_blank">Instagram</a>
-                    <a href="https://www.test.nl" target="_blank">LinkedIn</a>
-                    <a href="https://www.test.nl" target="_blank">Facebook</a>
-
-
+                <List title="Contact">
+                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="contact@novi.nl" />
+                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="030-3073200" />
+                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Newtonlaan 247, Utrecht"/>
+                    <ResourceLinks url="https://www.test.nl" target="_blank" classname="footer-icon" icon_only={true} icon="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-social-twitter-outline-1024.png" />
+                    <ResourceLinks url="https://www.test.nl" target="_blank" classname="footer-icon" icon_only={true} icon="https://cdn1.iconfinder.com/data/icons/social-media-outline-6/128/SocialMedia_Instagram-Outline-1024.png" />
+                    <ResourceLinks url="https://www.test.nl" target="_blank" classname="footer-icon" icon_only={true} icon="https://cdn2.iconfinder.com/data/icons/social-media-outline-1/32/linked_in_social_media_online-1024.png" />
+                    <ResourceLinks url="https://www.test.nl" target="_blank" classname="footer-icon" icon_only={true} icon="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Facebook_svg-1024.png"/>
                 </List>
-                <List
-                    title="Blijf op de hoogte">
+                <List title="Blijf op de hoogte">
                     <form>
                         <InputField type="text" placeholder="Voornaam" name="firstName" />
                         <InputField type="text" placeholder="E-mailadres" name="email" />
@@ -57,8 +45,8 @@ const Footer = () => {
                 </List>
             </div>
             <div>
-                <a href="https://www.test.nl" target="_blank">Privacy</a>
-                <a href="https://www.test.nl" target="_blank">Voorwaarden</a>
+                <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} />
+                <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} />
             </div>
         </div>
     );
