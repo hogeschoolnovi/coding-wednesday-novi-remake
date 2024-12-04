469fe5cd6d82df690f47fd80bc3cf746eb84b7b3
mjerta <maarten.postma1000@gmail.com>
2024-12-04 16:53:36 +0100

    final detais

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -3,7 +3,9 @@ import "./Testimonial.css"
 function Testimonial({img, title, text, linkText}) {
   return (
     <article className="testimonial-card">
-      <img src={img} alt=""/>
+      <div className="image-container">
+        <img src={img} alt=""/>
+      </div>
       <h3>{title}</h3>
       <p>{text}</p>
       <div className="link-box">
@@ -14,7 +16,6 @@ function Testimonial({img, title, text, linkText}) {
           id="arrow-right1_layer"><path
           d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path></g></svg>
       </span>
-
       </div>
     </article>
   )
