184fbefcd88e33eb4c1db69cf33944471ba8231a
RowanPl <Rowanplooij@hotmail.com>
2024-12-04 17:10:13 +0100

    updated the ResourceLinks

--- src/components/resourceLinks/ResourceLinks.jsx
+++ src/components/resourceLinks/ResourceLinks.jsx
@@ -3,7 +3,7 @@ import "./ResourceLinks.css"
 const ResourceLinks = ({icon, icon_alt, target, url, icon_only, name, classname}) => {
     return (
         <div>
-            <a href={url} target={target}>
+            <a className="resource-link" href={url} target={target}>
             {icon_only && icon &&
                 <img className={classname} src={icon} alt={icon_alt}/>
             }
