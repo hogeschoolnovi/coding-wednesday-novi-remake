20bdfb9c673278590c7d782d96672a513a53fb46
RowanPl <Rowanplooij@hotmail.com>
2024-12-04 16:29:16 +0100

    changed the ResourceLink to be more responsive

--- src/components/resourceLinks/ResourceLinks.jsx
+++ src/components/resourceLinks/ResourceLinks.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-const ResourceLinks = ({icon, icon_alt, target, url, icon_only}) => {
+const ResourceLinks = ({icon, icon_alt, target, url, icon_only, name}) => {
     return (
         <div>
-
-            {icon || icon_only &&
-            <img src={icon} alt={icon_alt}/>
+            {icon_only && icon &&
+                <img src={icon} alt={icon_alt}/>
             }
             {!icon_only &&
-            <a href={url} target={target}>Privacy</a>
+                <a href={url} target={target}>{name}</a>
             }
         </div>
     );
