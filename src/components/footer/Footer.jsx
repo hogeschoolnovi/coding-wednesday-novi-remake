6b624c54b750a489560aae6245f9ae4bea91352c
RowanPl <Rowanplooij@hotmail.com>
2024-12-05 15:19:23 +0100

    had to refactor the code to make it all work with the newly changed List and ResourceLinks

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,65 +2,122 @@ import React from 'react';
 import List from "../list/List.jsx";
 import ResourceLinks from "../resourceLinks/ResourceLinks.jsx";
 import "./Footer.css";
+import SectionContentWrapper from "../sectionContentWrapper/SectionContentWrapper.jsx";
+import {
+    Envelope,
+    FacebookLogo,
+    InstagramLogo,
+    LinkedinLogo,
+    MapPinLine,
+    PhoneCall,
+    TwitterLogo
+} from "@phosphor-icons/react";
 
 const Footer = () => {
+    const overNoviLinks = [
+        {url: "https://www.test.nl", label: "Opeidingen", isIconOnly: false},
+        {url: "https://www.test.nl", label: "Studeren Bij", isIconOnly: false},
+        {url: "https://www.test.nl", label: "Blog", isIconOnly: false},
+        {url: "https://www.test.nl", label: "Contact", isIconOnly: false},
+        {url: "https://www.test.nl", label: "Vacatures", isIconOnly: false}
+    ];
+
+    const studentenLinks = [
+        {url: "https://www.test.nl", label: "Kennisbank", isIconOnly: false},
+        {url: "https://www.test.nl", label: "Edhub", isIconOnly: false},
+        {url: "https://www.test.nl", label: "Subsidie", isIconOnly: false},
+        {url: "https://www.test.nl", label: "Verkort HBO-traject", isIconOnly: false}
+    ];
+
+    const inCompanyLinks = [
+        {url: "https://www.test.nl", label: "Business", isIconOnly: false},
+        {url: "https://www.test.nl", label: "Financiering", isIconOnly: false}
+    ];
+
+    const contactLinks = [
+        {
+            url: "https://www.test.nl",
+            label: "contact@novi.nl",
+            isIconOnly: false,
+            icon: <Envelope size={32} weight="duotone"/>
+        },
+        {
+            url: "https://www.test.nl",
+            label: "030-3073200",
+            isIconOnly: false,
+            icon: <PhoneCall size={32} weight="duotone"/>
+        },
+        {
+            url: "https://www.test.nl",
+            label: "Newtonlaan 247, Utrecht",
+            isIconOnly: false,
+            icon: <MapPinLine size={32} weight="duotone"/>
+        }
+    ];
+    const socialLinks = [{
+        url: "https://www.test.nl",
+        isIconOnly: true,
+        icon: <FacebookLogo size={32} weight="duotone"/>,
+        iconAlt: "Facebook logo"
+    },
+        {
+            url: "https://www.test.nl",
+            isIconOnly: true,
+            icon: <InstagramLogo size={32} weight="duotone"/>,
+            iconAlt: "Instagram logo"
+        },
+        {
+            url: "https://www.test.nl",
+            isIconOnly: true,
+            icon: <LinkedinLogo size={32} weight="duotone"/>,
+            iconAlt: "LinkedIn logo"
+        },
+        {
+            url: "https://www.test.nl",
+            isIconOnly: true,
+            icon: <TwitterLogo size={32} weight="duotone"/>,
+            iconAlt: "Twitter logo"
+        }
+    ];
+
     return (
-        <div>
-            <img src="https://www.novi.nl/hubfs/Website/NOVI%20logo%20wit.svg" alt="Logo of NOVI Hogeschool" className="footer-logo"/>
+        <SectionContentWrapper>
+            <img src="https://www.novi.nl/hubfs/Website/NOVI%20logo%20wit.svg" alt="Logo of NOVI Hogeschool"
+                 className="footer-logo"/>
             <div className="footer-main-container">
+                <section className="footer-sub-container footer-sub-left">
+                    <List title="Over NOVI" className="footer-list" links={overNoviLinks}/>
+                    <List title="Studenten" className="footer-list" links={studentenLinks}/>
+                    <List title="In Company" className="footer-list" links={inCompanyLinks}/>
+                </section>
+                <section className="footer-sub-container">
+                    <div>
+                        <List title="Contact" className="footer-list" links={contactLinks}/>
+
+                        <List
+                            isHorizontal={true}
+                            links={socialLinks}
+                        >
+                        </List>
+                    </div>
 
 
-            <section className="footer-sub-container">
-                <List title="Over NOVI">
-                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Opeidingen" />
-                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Studeren Bij" />
-                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Blog" />
-                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Contact" />
-                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Vacatures"/>
-                </List>
-                <List title="Studenten">
-                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Kennisbank" />
-                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Edhub"/>
-                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Subsidie"/>
-                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Verkort HBO-traject"/>
-                </List>
-                <List title="In Company">
-                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Business" />
-                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Financiering"/>
-                </List>
-            </section>
-            <section className="footer-sub-container">
-                <List title="Contact">
-                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="contact@novi.nl" />
-                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="030-3073200" />
-                    <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} name="Newtonlaan 247, Utrecht"/>
-                   <div className="footer-icon-container">
-                       <ResourceLinks url="https://www.test.nl" target="_blank" classname="footer-icon" icon_only={true} icon="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-social-twitter-outline-1024.png" />
-                       <ResourceLinks url="https://www.test.nl" target="_blank" classname="footer-icon" icon_only={true} icon="https://cdn1.iconfinder.com/data/icons/social-media-outline-6/128/SocialMedia_Instagram-Outline-1024.png" />
-                       <ResourceLinks url="https://www.test.nl" target="_blank" classname="footer-icon" icon_only={true} icon="https://cdn2.iconfinder.com/data/icons/social-media-outline-1/32/linked_in_social_media_online-1024.png" />
-                       <ResourceLinks url="https://www.test.nl" target="_blank" classname="footer-icon" icon_only={true} icon="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Facebook_svg-1024.png"/>
-
-                   </div>
-                </List>
-                    <List title="Blijf op de hoogte"
-
-                    >
+                    <List title="Blijf op de hoogte" className="footer-list">
                         <form className="footer-form-container">
-                            <input type="text" placeholder="Voornaam" name="firstName" />
-                            <input type="text" placeholder="E-mailadres" name="email" />
+                            <input type="text" placeholder="Voornaam" name="firstName"/>
+                            <input type="text" placeholder="E-mailadres" name="email"/>
                             <button type="submit">Aanmelden nieuwsbrief</button>
                         </form>
                     </List>
-            </section>
 
-            <div>
-                <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} />
-                <ResourceLinks url="https://www.test.nl" target="_blank" icon_only={false} />
+                </section>
+
+                <div>
+                    <ResourceLinks url="https://www.test.nl" target="_blank" isOnlyIcon={false}/>
+                    <ResourceLinks url="https://www.test.nl" target="_blank" isOnlyIcon={false}/>
+                </div>
             </div>
-
-        </div>
-
-        </div>
+        </SectionContentWrapper>
     );
 };
 
