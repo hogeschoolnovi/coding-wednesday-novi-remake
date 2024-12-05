b30789dadf6bec117da090badfcba72373bba3a5
RowanPl <Rowanplooij@hotmail.com>
2024-12-05 15:18:52 +0100

    made it so the list will map over links

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,12 +1,28 @@
+// Update List.jsx
 import React from 'react';
 import "./List.css";
-const List = ({title, children, classname}  ) => {
+import ResourceLinks from '../resourceLinks/ResourceLinks';
+
+const List = ({title, links, variant, children, isHorizontal}) => {
+    let className = variant === 'dark' ? 'list-dark' : 'list-light';
+    className += isHorizontal ? ' list-horizontal' : ' list-vertical';
     return (
-        <div className={classname}>
-            <h3> {title} </h3>
+        <div className={className}>
+            <h3>{title}</h3>
+            {links && links.map((link, index) => (
+                <ResourceLinks
+                    key={index}
+                    url={link.url}
+                    label={link.label}
+                    icon={link.icon}
+                    iconAlt={link.iconAlt}
+                    target={link.isIconOnly ? "_blank" : "_self"}
+                    isOnlyIcon={!!link.icon}
+                    className={link.className}
+                />
+            ))}
             {children}
         </div>
     );
 };
-
 export default List;
