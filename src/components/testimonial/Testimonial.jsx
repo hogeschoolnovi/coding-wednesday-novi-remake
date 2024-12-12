2d8afd851e5a0a391d71e1eff6e3c8635ef9f9ba
Maarten Postma <maarten.postma1000@gmail.com>
2024-12-12 17:16:13 +0100

    Starting to create vimeo component

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,28 +1,33 @@
 import "./Testimonial.css"
 import PlayButton from "../playbutton/PlayButton.jsx";
-import { ArrowRight } from "@phosphor-icons/react"
+import {ArrowRight} from "@phosphor-icons/react"
+
 function Testimonial({img, title, text, altText}) {
-  return (<article className="testimonial-card">
-    <a href="#">
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+  return (
+    <article className="testimonial-card">
       <div className="image-container">
         <img src={img} alt={altText}/>
         <PlayButton
           className={'variant-img-container'}
         />
       </div>
-    </a>
-    <p className={'video-card-title'}>{title}</p>
-    <p className={'testimonial-text'}>{text}</p>
-    <div className="link-box">
-      <PlayButton
-        className={'variant-link-box'}
-      />
-      <p>Bekijk {title}&#39;s verhaal</p>
-      <span>
-        <ArrowRight size={36} height={20} weight="bold" />
+      <p className={'video-card-title'}>{title}</p>
+      <p className={'testimonial-text'}>{text}</p>
+      <div className="link-box">
+        <PlayButton
+          className={'variant-link-box'}
+        />
+        <p>Bekijk {title}&#39;s verhaal</p>
+        <span>
+        <ArrowRight size={36} height={20} weight="bold"/>
       </span>
-    </div>
-  </article>)
+      </div>
+    </article>)
 }
 
 export default Testimonial;
