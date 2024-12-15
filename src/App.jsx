fd77a17d79c9ea0ec8ef2a8b4196374808faa9c8
MorikuMarieke <mariekevdstaaij@gmail.com>
2024-12-15 14:42:25 +0100

    processed Novas feedback on pullrequest, made some minor changes on lay out that do not impact overall look

--- src/App.jsx
+++ src/App.jsx
@@ -18,29 +18,28 @@ function App() {
                 <section className="courses-section">
                     <SectionContentWrapper>
                         <h2>Onze hbo ICT-opleidingen</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut illum impedit inventore iste
-                            nostrum omnis quae, quam quis rem ut.</p>
+                        <p>Tussen onze brede deeltijdopleidingen zit altijd wel een traject dat aansluit op jouw droombaan.</p>
 
-                        <div className="info-card-wrapper">
+                        <div className="info-cards-container">
                             <InformationCard
-                                infoCardTitle="Titel"
+                                infoCardTitle="Bedrijfskundige informatica"
                                 infoCardContent="Wil je leren hoe een netwerk in elkaar zit – én hoe je netwerkt? Met deze allround-ICT-opleiding rol je zo het bedrijfsleven in."
                                 experienceNeeded={true}
                             />
                             <InformationCard
-                                infoCardTitle="Titel"
-                                infoCardContent="Banaan peer banaan banaan banaan en andere tekst"
+                                infoCardTitle="Software development"
+                                infoCardContent="Wil je leren hoe een netwerk in elkaar zit – én hoe je netwerkt? Met deze allround-ICT-opleiding rol je zo het bedrijfsleven in."
                                 experienceNeeded={false}
                             />
                             <InformationCard
-                                infoCardTitle="Titel"
-                                infoCardContent="Banaan banaan banaan kumquat banaan joepie"
+                                infoCardTitle="Cyber security"
+                                infoCardContent="Deze studierichting is misschien wel het meest dynamische vakgebied in de ICT. Hoe bescherm je een organisatie tegen cybercriminelen?"
                                 experienceNeeded={true}
                                 onClick="hier-link-naar-pagina"
                             />
                             <InformationCard
-                                infoCardTitle="Titel"
-                                infoCardContent="Banaan banaan banaan kumquat banaan joepie"
+                                infoCardTitle="Geo Business Intelligence"
+                                infoCardContent="Locatiedata zijn tegenwoordig big business. Met deze studierichting breng je de wereld om je heen letterlijk in kaart."
                                 experienceNeeded={true}
                                 onClick="hier-link-naar-pagina"
                             />
@@ -50,7 +49,7 @@ function App() {
                         <h2>ICT-Bootcamps</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut illum impedit inventore iste
                             nostrum omnis quae, quam quis rem ut.</p>
-                        <div className="info-card-wrapper">
+                        <div className="info-cards-container">
                             <InformationCard
                                 infoCardTitle="Titel"
                                 infoCardContent="Banaan banaan banaan kumquat banaan joepie"
@@ -87,7 +86,7 @@ function App() {
                         <h2>ICT-Cursussen</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut illum impedit inventore iste
                             nostrum omnis quae, quam quis rem ut.</p>
-                        <div className="info-card-wrapper">
+                        <div className="info-cards-container">
                             <InformationCard
                                 infoCardTitle="Titel"
                                 infoCardContent="Banaan banaan banaan kumquat banaan joepie"
--- src/components/informationCard/InformationCard.jsx
+++ src/components/informationCard/InformationCard.jsx
@@ -4,8 +4,8 @@ import {ArrowRight} from "@phosphor-icons/react";
 
 const InformationCard = ({infoCardTitle, infoCardContent, experienceNeeded, onClick}) => {
     return (
-        <div className="infoCard">
-            <h3 className="info-card-title">{infoCardTitle}</h3>
+        <article className="info-card">
+            <h4 className="info-card-title">{infoCardTitle}</h4>
             <p className="info-card-content">{infoCardContent}</p>
             {experienceNeeded && <p className="info-card-experience-tag"><em>Enige IT-ervaring vereist</em></p>}
             <button
@@ -15,7 +15,7 @@ const InformationCard = ({infoCardTitle, infoCardContent, experienceNeeded, onCl
             >
                 <ArrowRight size={20} />
             </button>
-        </div>
+        </article>
     );
 };
 
